49a6a9cd5315a88e22ed903606f029191fd6ca97
“Tiago <tiago.steiner@gruporbs.com.br>
2016-06-27 21:41:22 -0300

    criado modelo de dados

--- src/main/resources/db/migration/V1__DDL.sql
+++ src/main/resources/db/migration/V1__DDL.sql
@@ -17,17 +17,101 @@ SET NAMES latin1;
 CREATE SCHEMA IF NOT EXISTS infacademics COLLATE utf8_general_ci ;
 
 ALTER DATABASE infacademics
-DEFAULT CHARACTER SET utf8
-DEFAULT COLLATE utf8_general_ci;
+	DEFAULT CHARACTER SET utf8
+	DEFAULT COLLATE utf8_general_ci;
 
-CREATE TABLE book (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `book` VARCHAR(200) NULL,
-  `country` VARCHAR(80) NULL,
-  `state` VARCHAR(20) NULL,
-  `autor` VARCHAR(150) NULL,
-  PRIMARY KEY (`id`)
-)DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;;
+CREATE TABLE IF NOT EXISTS `infacademics`.`pesquisador` (
+  `idpesquisador` INT NOT NULL AUTO_INCREMENT,
+  `nomepesquisador` VARCHAR(45) NOT NULL,
+  `datacadastro` DATETIME NULL,
+  PRIMARY KEY (`idpesquisador`))
+ENGINE = InnoDB;
 
-INSERT INTO book VALUES (1,'A volta dos que não foram', 'Brasil', 'RS', 'Fulano de tal');
-INSERT INTO book VALUES (2,'O regresso', 'Estados Unidos', 'NY', 'Ciclano de Sousa');
+
+-- -----------------------------------------------------
+-- Table `infacademics`.`Usuario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `infacademics`.`usuario` (
+  `nomeusuario` VARCHAR(50) NOT NULL,
+  `ativo` TINYINT(1) NULL,
+  `datacriacao` DATETIME NULL,
+  `idpesquisador` INT NOT NULL,
+  UNIQUE INDEX `NomeUsuario_UNIQUE` (`nomeusuario` ASC),
+  INDEX `fk_Usuario_Pesquisador_idx` (`idpesquisador` ASC),
+  PRIMARY KEY (`idpesquisador`),
+  CONSTRAINT `fk_Usuario_pesquisador`
+    FOREIGN KEY (`idpesquisador`)
+    REFERENCES `infacademics`.`pesquisador` (`idpesquisador`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `infacademics`.`TipoPublicacao`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `infacademics`.`tipopublicacao` (
+  `idtipopublicacao` INT NOT NULL,
+  `nometipopublicacao` VARCHAR(45) NULL,
+  PRIMARY KEY (`idtipopublicacao`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `infacademics`.`Publicacao`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `infacademics`.`publicacao` (
+  `idpublicacao` INT NOT NULL,
+  `idtipopublicacao` INT NOT NULL,
+  `nomepublicacao` VARCHAR(400) NULL,
+  `datacadastro` DATETIME NULL,
+  `atributos` TEXT NULL,
+  `ano` INT NULL,
+  `publicador` VARCHAR(400) NULL,
+  PRIMARY KEY (`idpublicacao`),
+  INDEX `fk_publicacao_tipopublicacao1_idx` (`idtipopublicacao` ASC),
+  CONSTRAINT `fk_Publicacao_TipoPublicacao1`
+    FOREIGN KEY (`idtipopublicacao`)
+    REFERENCES `infacademics`.`tipopublicacao` (`idtipopublicacao`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `infacademics`.`PublicacaoCampos`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `infacademics`.`publicacaocampos` (
+  `idtpublicacaocampos` INT NOT NULL,
+  `nomecampo` TEXT NULL,
+  `idpublicacao` INT NOT NULL,
+  PRIMARY KEY (`idtpublicacaocampos`),
+  INDEX `fk_publicacaocampos_publicacao1_idx` (`idpublicacao` ASC),
+  CONSTRAINT `fk_publicacaocampos_publicacao1`
+    FOREIGN KEY (`idpublicacao`)
+    REFERENCES `infacademics`.`publicacao` (`idpublicacao`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `infacademics`.`PublicacaoPesquisador`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `infacademics`.`publicacaopesquisador` (
+  `idpublicacao` INT NOT NULL,
+  `idpesquisador` INT NOT NULL,
+  PRIMARY KEY (`idpublicacao`, `idpesquisador`),
+  INDEX `fk_publicacao_has_pesquisador_pesquisador1_idx` (`idpesquisador` ASC),
+  INDEX `fk_publicacao_has_pesquisador_publicacao1_idx` (`idpublicacao` ASC),
+  CONSTRAINT `fk_Publicacao_has_pesquisador_publicacao1`
+    FOREIGN KEY (`idpublicacao`)
+    REFERENCES `infacademics`.`publicacao` (`idpublicacao`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_publicacao_has_pesquisador_pesquisador1`
+    FOREIGN KEY (`idpesquisador`)
+    REFERENCES `infacademics`.`pesquisador` (`idpesquisador`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
