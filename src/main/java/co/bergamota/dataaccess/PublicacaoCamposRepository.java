26aecf7542ebc19fa16cb0dd0ad1bad45d21fdba
“Tiago <tiago.steiner@gruporbs.com.br>
2016-06-28 00:54:31 -0300

    Incluindo ajustes no repository
    Incluindo ajustes no repository

---
+++ src/main/java/co/bergamota/dataaccess/PublicacaoCamposRepository.java
@@ -0,0 +1,8 @@
+package co.bergamota.dataaccess;
+
+import co.bergamota.business.objects.PublicacaoCampos;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublicacaoCamposRepository extends CrudRepository<PublicacaoCampos, Long> {
+
+}
--- src/main/java/co/bergamota/dataaccess/PublicacaoRepository.java
+++ src/main/java/co/bergamota/dataaccess/PublicacaoRepository.java
@@ -1,7 +1,6 @@
 package co.bergamota.dataaccess;
 
 import co.bergamota.business.objects.Publicacao;
-import co.bergamota.business.objects.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PublicacaoRepository extends CrudRepository<Publicacao, Long> {
