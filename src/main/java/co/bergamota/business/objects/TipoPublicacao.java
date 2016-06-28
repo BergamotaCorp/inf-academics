da2f3b9bfeb096229e1b28aa4881d3caa98183db
“Tiago <tiago.steiner@gruporbs.com.br>
2016-06-28 00:33:56 -0300

    Mais classes de entidades
    Mais classes de entidades

--- src/main/java/co/bergamota/business/objects/TipoPublicacao.java
+++ src/main/java/co/bergamota/business/objects/TipoPublicacao.java
@@ -4,7 +4,6 @@ package co.bergamota.business.objects;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.Date;
 
 @Entity
 @Table(name = "tipopublicacao")
